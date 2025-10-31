3fc57763c749d414b1811d5839907bbefd436bbf
Alara Engin <alara.engin2008@gmail.com>
2025-10-31 09:38:47 -0700

    added test cases

--- Main.java
+++ Main.java
@@ -20,8 +20,9 @@ public class Main{
    public static void main(String[] args){
       // Create an object to call the methods
       Main date = new Main();
-      System.out.print(date.main(8, 1, 3, 2));
-      System.out.print(date.main(8, 1, 3, 2));
-      
+      System.out.println(date.main(6, 3, 9, 20));
+      System.out.println(date.main(10, 1, 2, 5));
+      System.out.println(date.main(8, 15, 8, 15));
+      System.out.println(date.main(8, 15, 8, 16));
       }
 }
