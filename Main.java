8544719c6310bd2afdfccc3d786c2778a29e7e92
Alara Engin <alara.engin2008@gmail.com>
2025-10-31 09:36:26 -0700

    added comments

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main{
+     //Method that turns m integer to String months.
      public String monthToString(int m){
       String[] months = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
       return (months[m-1]);
      }
      // Format neeeded: June 3 comes before Sep 20
+     // The method that checks what comes before
      public String main(int m1, int d1, int m2, int d2){
       if (m1 < m2){
          return(monthToString(m1) + " " + d2 + " comes before " + monthToString(m2) + " " + d2);
@@ -16,8 +18,10 @@ public class Main{
       }
    }
    public static void main(String[] args){
+      // Create an object to call the methods
       Main date = new Main();
       System.out.print(date.main(8, 1, 3, 2));
-      //test
-   }
+      System.out.print(date.main(8, 1, 3, 2));
+      
+      }
 }
