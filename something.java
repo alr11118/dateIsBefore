4eac0563f564455f87f99bab20a65db0aea5db66
Alara Engin <alara.engin2008@gmail.com>
2025-11-03 09:16:15 -0800

    made it simpler

--- something.java
+++ something.java
@@ -1,12 +1,12 @@
 public class something{
      //Method that turns m integer to String months.
-     public String monthToString(int m){
+     public static String monthToString(int m){
       String[] months = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
       return (months[m-1]);
      }
      // Format neeeded: June 3 comes before Sep 20
      // The method that checks what comes before
-     public String main(int m1, int d1, int m2, int d2){
+     public static String main(int m1, int d1, int m2, int d2){
       if (m1 > 12 || m2 > 12 || d1 > 31 || d2 > 31){
          return("Invalid month or day");
       }
@@ -21,12 +21,11 @@ public class something{
       }
    }
    public static void main(String[] args){
-      // Create an object to call the methods
-      something date = new something();
-      System.out.println(date.main(6, 3, 9, 20));
-      System.out.println(date.main(10, 1, 2, 5));
-      System.out.println(date.main(8, 15, 8, 15));
-      System.out.println(date.main(8, 15, 8, 16));
-      System.out.println(date.main(40, 150, 89, 165));
+      // Test scenarios
+      System.out.println(main(6, 3, 9, 20));
+      System.out.println(main(10, 1, 2, 5));
+      System.out.println(main(8, 15, 8, 15));
+      System.out.println(main(8, 15, 8, 16));
+      System.out.println(main(40, 150, 89, 165));
       }
 }
