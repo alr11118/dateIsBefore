59e2a486bf56ff5cf00ce7da34c3d08272999123
Alara Engin <alara.engin2008@gmail.com>
2025-11-03 09:34:30 -0800

    added feature
    Now checks for how many days a month has then uses it to decide if the date is invalid.

--- something.java
+++ something.java
@@ -1,4 +1,25 @@
 public class something{
+     public static int monthHasHowManyDays(int m){
+      if (m==2){
+         return (28);
+      }
+      else if (m<=7){
+         if (m%2 == 1){
+            return(31);
+         }
+         else{
+            return 30;
+         }
+      }
+      else{
+         if (m%2 == 0){
+            return(31);
+         }
+         else{
+            return 30;
+         }
+      }
+     }
      //Method that turns m integer to String months.
      public static String monthToString(int m){
       String[] months = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
@@ -7,7 +28,7 @@ public class something{
      // Format neeeded: June 3 comes before Sep 20
      // The method that checks what comes before
      public static String dateIsBefore(int m1, int d1, int m2, int d2){
-      if (m1 > 12 || m2 > 12 || d1 > 31 || d2 > 31){
+      if (m1 > 12 || m2 > 12 || d1 > monthHasHowManyDays(m1) || d2 > monthHasHowManyDays(m2)){
          return("Invalid month or day");
       }
       else if (m1 < m2){
@@ -27,5 +48,6 @@ public class something{
       System.out.println(dateIsBefore(8, 15, 8, 15));
       System.out.println(dateIsBefore(8, 15, 8, 16));
       System.out.println(dateIsBefore(40, 150, 89, 165));
+      System.out.println(dateIsBefore(6, 31, 9, 20));
       }
 }
