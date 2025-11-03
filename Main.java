cd8f8bd42861bd37e8e4d75c2618bf93ffcbecba
Alara Engin <alara.engin2008@gmail.com>
2025-11-03 09:12:02 -0800

    removed the unnecesary object

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
      // The method that checks what comes before
-     public boolean main(int m1, int d1, int m2, int d2){
+     public static boolean main(int m1, int d1, int m2, int d2){
       if (m1 < m2){
          return(true);
       }
@@ -8,15 +8,14 @@ public class Main{
          return(true);
       }
       else{
-         return(false);
+         return(false); 
       }
    }
    public static void main(String[] args){
       // Create an object to call the methods
-      Main date = new Main();
-      System.out.println(date.main(6, 3, 9, 20));
-      System.out.println(date.main(10, 1, 2, 25));
-      System.out.println(date.main(8, 15, 8, 15));
-      System.out.println(date.main(8, 15, 8, 16));
+      System.out.println(main(6, 3, 9, 20));
+      System.out.println(main(10, 1, 2, 25));
+      System.out.println(main(8, 15, 8, 15));
+      System.out.println(main(8, 15, 8, 16));
       }
 }
